7c2e7ee5eb7b50d648187335b4eeadaa0d371ddc
TheAnushervon <anushervon4j@gmail.com>
2024-06-14 01:35:50 +0300

    signup made

--- rag_backend/authentication/views.py
+++ rag_backend/authentication/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import redirect, render
 from .models import User
 from django.contrib import messages 
+from django.contrib.auth import authenticate, login
+
 # Create your views here.
 def index(request): 
     return render(request, 'authentication/index.html')
@@ -13,9 +15,10 @@ def signup (request):
         email = request.POST['email']
         pass1 = request.POST['pass1']
         pass2 = request.POST['pass2']
-        myuser = User.objects.create(username = username, email=email, password = pass1)
-        myuser.first_name = fname 
-        myuser.last_name =  lname 
+        telegram_id = request.POST['telegram-id']
+        myuser = User.objects.create(username = username, email=email, password = pass1, 
+                                     first_name = fname, last_name = lname, telegram_id = telegram_id )
+        
         myuser.save()
         messages.success(request, 'Your account have been successfully created.')
         return redirect('signin')
@@ -23,6 +26,17 @@ def signup (request):
 
 
 def signin (request): 
+    if request.method == 'POST': 
+        username = request.POST['username']
+        pass1 = request.POST['pass1']
+        user = authenticate(username=username, password = pass1)
+        if user is not None: 
+            login(request, user)
+            fname = user.firstname 
+            return render(request, "authentication/index.html", {'fname': fname})
+        else: 
+            messages.error(request, "Bad credentials")
+            return redirect('index')
     return render (request, 'authentication/signin.html')
 
 
