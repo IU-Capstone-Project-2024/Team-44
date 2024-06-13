98828b65b9268c7d6bd0159e8e792bcd83bcfd04
TheAnushervon <anushervon4j@gmail.com>
2024-06-14 00:34:05 +0300

    signup page being configured

--- rag_backend/authentication/models.py
+++ rag_backend/authentication/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class User(models.Model): 
+    username = models.CharField(max_length=50)
+    email = models.EmailField()
+    password = models.CharField(max_length=50)
+    telegram_id = models.CharField(max_length=20)
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    
+    
--- rag_backend/authentication/views.py
+++ rag_backend/authentication/views.py
@@ -1,5 +1,32 @@
-from django.shortcuts import render
-
+from django.shortcuts import redirect, render
+from .models import User
+from django.contrib import messages 
 # Create your views here.
-def check(request): 
-    return render(request, 'check.html')
+def index(request): 
+    return render(request, 'authentication/index.html')
+
+def signup (request): 
+    if request.method == "POST": 
+        username = request.POST['username']
+        fname = request.POST['fname']
+        lname = request.POST['lname']
+        email = request.POST['email']
+        pass1 = request.POST['pass1']
+        pass2 = request.POST['pass2']
+        myuser = User.objects.create(username = username, email=email, password = pass1)
+        myuser.first_name = fname 
+        myuser.last_name =  lname 
+        myuser.save()
+        messages.success(request, 'Your account have been successfully created.')
+        return redirect('signin')
+    return render (request, 'authentication/signup.html')
+
+
+def signin (request): 
+    return render (request, 'authentication/signin.html')
+
+
+def signout (request): 
+    return render (request, 'authentication/signout.html')
+
+ 
