8669de5aa85c8e396ae61c741a21e2da04035736
TheAnushervon <anushervon4j@gmail.com>
2024-06-15 20:45:06 +0300

    Confirmation of signup finished

--- rag_backend/authentication/views.py
+++ rag_backend/authentication/views.py
@@ -6,6 +6,13 @@ from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.hashers import make_password, check_password
 from rag_backend import settings 
 from django.core.mail import send_mail
+from django.contrib.sites.shortcuts import get_current_site
+from django.template.loader import render_to_string
+from base64 import urlsafe_b64encode
+from django.utils.encoding import force_bytes, force_str 
+from . tokens import generate_token
+from django.core.mail import EmailMessage, send_mail
+from django.utils.http import urlsafe_base64_decode, urlsafe_base64_encode
 # Create your views here.
 def index(request): 
     return render(request, 'authentication/index.html')
@@ -35,16 +42,33 @@ def signup(request):
                 myuser = User.objects.create(username = username, email=email, 
                                              password = pass1, first_name=fname, last_name = lname, telegram_id = telegram_id)
                 
+                myuser.is_active = False 
                 myuser.save()
                 messages.success(request, 'Your account has been successfully created.We have send you confirmation email, please confirm your email in order to activate your account')
                 
                 #Welcome message using email 
                 subject = "Welcome to Quiz generator"
-                message = "Hello" + myuser.first_name + "!! \n" + "Welcome to Quiz generator \n We have also sent you a confirmation email, please confirm your email ";
+                message = "Hello " + myuser.first_name + "!! \n" + "Welcome to Quiz generator \n We have also sent you a confirmation email, please confirm your email ";
                 from_email = settings.EMAIL_HOST_USER   
                 to_list = [myuser.email]
                 send_mail(subject, message, from_email, to_list, fail_silently=True)
                 
+                #Confirmation email 
+                current_site = get_current_site(request)
+                email_subject = "Confirm your email @Quiz Generator"
+                message2 = render_to_string('email_confirmation.html', 
+                                            {'name': myuser.first_name, 
+                                             'domain': current_site.domain, 
+                                             'uid': urlsafe_base64_encode(force_bytes(myuser.pk)), 
+                                             'token': generate_token.make_token(myuser), })
+                email = EmailMessage(
+                    email_subject, 
+                    message2, 
+                    settings.EMAIL_HOST_USER, 
+                    [myuser.email], 
+                )
+                email.fail_silently = True
+                email.send()
                 return redirect('index')
         else:
             messages.info(request, 'Passwords do not match')
@@ -91,4 +115,23 @@ def signout (request):
     messages.success(request, "Successfully logged out")
     return render (request, 'authentication/signout.html')
 
-   
+def activate(request, uidb64, token): 
+    print("before") 
+    try: 
+        print(uidb64)
+        uid = force_str(urlsafe_base64_decode(uidb64))
+        print(uid)
+        myuser = User.objects.get(pk=uid)
+        
+        print(myuser)
+    except (TypeError, ValueError, OverflowError, User.DoesNotExist): 
+        print("there")
+        myuser = None
+    
+    if myuser is not None and generate_token.check_token(myuser, token): 
+        myuser.is_active = True
+        myuser.save()
+        login(request, myuser)
+        return redirect('index') 
+    else : 
+        return render(request, 'activation_failed.html')
