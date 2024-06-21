afc7c6619025e05ad38cf3d38a66c4cda5bc4edd
TheAnushervon <anushervon4j@gmail.com>
2024-06-21 21:07:13 +0300

    router:tested ml_view and add_docs(errors)

--- rag_backend/app1/views.py
+++ rag_backend/app1/views.py
@@ -17,6 +17,7 @@ from django.utils.encoding import force_bytes, force_str
 from django.core.mail import EmailMessage, send_mail
 from django.utils.http import urlsafe_base64_decode, urlsafe_base64_encode
 
+from langchain_core.documents import Document
 
 @csrf_exempt
 def ml_view(request):
@@ -29,9 +30,27 @@ def ml_view(request):
 
         print(query)
         router = Router()
+        #error : page_content method is not in tuple 
+        doc = Document(page_content="This is the content of the document.", metadata={"source": "example.com"}) 
+        documents = [doc]
+        verdict = router.add_docs(documents)
         result = router.retrieve(query, 'similarity', {'k': 1})
-        return JsonResponse({'result': result})
+        results = [
+            {'result': result}, 
+            {'document_isAdded': verdict}, 
+        ]
+        return JsonResponse(results)
 
+@csrf_exempt
+def add_docs(request):
+    router = Router() 
+    doc = Document(page_content="This is the content of the document.", metadata={"source": "example.com"}) 
+    documents = [doc]
+    verdict = router.add_docs(documents)
+    result = [{'result': verdict}]
+    return JsonResponse(result) 
 
 def main(request):
     return render(request, 'main.html')
+
+
